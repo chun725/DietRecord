2e2fd43dc7c6e1aaaa7ed5766a919d7b428a41c1
Chun Chen <chibo0725@gmail.com>
2022-11-21 20:31:26 +0800

    feat: Add header refresh in the report page

--- DietRecord/DietRecord/Controller/Report/ReportVC.swift
+++ DietRecord/DietRecord/Controller/Report/ReportVC.swift
@@ -11,6 +11,7 @@ class ReportVC: UIViewController, UITableViewDataSource {
     @IBOutlet weak var reportTableView: UITableView!
     @IBOutlet weak var dateTextField: UITextField!
     
+    var refreshControl: UIRefreshControl?
     let reportProvider = ReportProvider()
     var weeklyDietRecord: [FoodDailyInput]? {
         didSet {
@@ -23,9 +24,13 @@ class ReportVC: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         dateTextField.text = dateFormatter.string(from: Date())
-        fetchWeeklyDiet()
+        fetchWeeklyDiet(sender: nil)
         reportTableView.dataSource = self
         reportTableView.registerCellWithNib(identifier: ReportDetailCell.reuseIdentifier, bundle: nil)
+        refreshControl = UIRefreshControl()
+        guard let refreshControl = refreshControl else { return }
+        reportTableView.addSubview(refreshControl)
+        refreshControl.addTarget(self, action: #selector(fetchWeeklyDiet), for: .valueChanged)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,20 +46,23 @@ class ReportVC: UIViewController, UITableViewDataSource {
             chooseDatePage.closure = { [weak self] date in
                 if self?.dateTextField.text != date {
                     self?.dateTextField.text = date
-                    self?.fetchWeeklyDiet()
+                    self?.fetchWeeklyDiet(sender: nil)
                 }
             }
             self.present(chooseDatePage, animated: false)
         }
     }
     
-    @objc func fetchWeeklyDiet() {
-        LKProgressHUD.show()
+    @objc func fetchWeeklyDiet(sender: UIRefreshControl?) {
+        if sender == nil {
+            LKProgressHUD.show()
+        }
         isLoading = true
         guard let dateString = dateTextField.text,
             let date = dateFormatter.date(from: dateString)
         else { return }
         reportProvider.fetchWeeklyDietRecord(date: date) { result in
+            self.refreshControl?.endRefreshing()
             switch result {
             case .success(let data):
                 self.isLoading = false

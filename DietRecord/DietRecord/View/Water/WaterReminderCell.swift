0bcdcce8a0dd5550b105229ed189ae88ead94f7b
Chun Chen <chibo0725@gmail.com>
2022-11-23 20:20:07 +0800

    fix: Modify the water reminder cell in the water page and the date picker in the water input page

--- DietRecord/DietRecord/View/Water/WaterReminderCell.swift
+++ DietRecord/DietRecord/View/Water/WaterReminderCell.swift
@@ -11,8 +11,12 @@ class WaterReminderCell: UITableViewCell {
     @IBOutlet weak var reminderBackgroundView: UIView!
     @IBOutlet weak var deleteButton: UIButton!
     @IBOutlet weak var reminderTimeLabel: UILabel!
+    @IBOutlet weak var timeImageView: UIImageView!
     
     func layoutCell(time: String) {
+        let timeString = time.components(separatedBy: ":")
+        guard let hour = Int(timeString[0]) else { return }
+        timeImageView.image = hour < 18 && hour >= 6 ? UIImage(named: "Image_Sun") : UIImage(named: "Image_Night")
         reminderBackgroundView.setShadowAndRadius(radius: 10)
         reminderTimeLabel.text = time
     }
